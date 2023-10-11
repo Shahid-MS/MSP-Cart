2e3ebe68162b936450b66f2be51c5dcc35249ea4
Md Shahid Alam <md.shahidalam.cse24@heritageit.edu.in>
2023-10-11 21:30:01 +0530

    First Commit

---
+++ src/customer/Home carousel/MainCarouselData.js
@@ -0,0 +1,6 @@
+export const MainCarouselData =[
+    {
+        image:"",
+        path:""
+    },
+]
---
+++ src/customer/Pages/HomePage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../Home carousel/MainCarousel';
+
+const Homepage = () => {
+  return (
+    <>
+     <div>
+      <MainCarousel/>
+    </div>
+    </>
+   
+  )
+}
+
+export default Homepage;
